c482fca1ede795c451b3d0ea10530df010a5b3e5
Shaayer <qalam0720@gmail.com>
2024-09-03 16:35:17 -0700

    y camera zooming

--- Control.gd
+++ Control.gd
@@ -9,22 +9,46 @@ var posPlayer2y;
 func _ready():
 	pass # Replace with function body.
 
-var topCameraDistance=1000
+var topCameraDistancex=1000
+var xZoom=0
+var yZoom=0
+var topCameraDistancey=550
+var xdistance=0
+var ydistance=0
+var dropDistance=1000
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	posPlayer2=$Player2.position.x
 	posPlayer1=$Player.position.x
 	posPlayer2y=$Player2.position.y
 	posPlayer1y=$Player.position.y
+	if posPlayer1y>dropDistance:
+		posPlayer1y=dropDistance
+	if posPlayer2y>dropDistance:
+		posPlayer2y=dropDistance
 	$Camera2D.cameraCenter(posPlayer1,posPlayer2)
 	$Camera2D.cameraCentery(posPlayer1y,posPlayer2y)
-	distance=findDistance()
-	if distance>topCameraDistance:
-		$Camera2D.zoom.x=topCameraDistance/distance
-		$Camera2D.zoom.y=topCameraDistance/distance
+	xdistance=abs(posPlayer2-posPlayer1)
+	ydistance=abs(posPlayer2y-posPlayer1y)
+	if xdistance>topCameraDistancex:
+		xZoom=topCameraDistancex/xdistance
 	else:
-		$Camera2D.zoom.x=1
-		$Camera2D.zoom.y=1
+		xZoom=1
+		
+	if ydistance>topCameraDistancey:
+		yZoom=topCameraDistancey/ydistance
+	else:
+		yZoom=1
+	
+	if yZoom<xZoom:
+		$Camera2D.zoom.x=yZoom
+		$Camera2D.zoom.y=yZoom
+	elif xZoom<yZoom:
+		$Camera2D.zoom.x=xZoom
+		$Camera2D.zoom.y=xZoom
+	elif xZoom==yZoom:
+		$Camera2D.zoom.x=xZoom
+		$Camera2D.zoom.y=xZoom
 	
 func findDistance():
 	var x=abs(posPlayer2-posPlayer1)
